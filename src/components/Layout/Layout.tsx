df110d1581306f848502ba17a0ae2b1ec9596ef5
Prabhjot Singh <prabhjot.singh@dream11.com>
2025-11-26 15:05:17 +0530

    chore: fix padding

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -44,7 +44,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           component="main"
           sx={{
             flexGrow: 1,
-            padding: theme.customSpacing.contentPadding,
             marginLeft: theme.customSpacing.sidebarWidth,
             overflowY: "auto",
             height: `calc(100vh - ${theme.customSpacing.appBarHeight})`,
