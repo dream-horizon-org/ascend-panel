fd9bb2385895b7ccf2dc9d005a27e1ab43c33cf7
Harsh Mishra <harsh.mishra@dream11.com>
2025-11-27 17:21:28 +0530

    fix: prettier write

--- src/pages/CreateExperiment/index.tsx
+++ src/pages/CreateExperiment/index.tsx
@@ -170,7 +170,6 @@ const CreateExperiment = () => {
               label="Experiment ID"
               placeholder="Enter experiment id"
               infoText="Unique identifier for the experiment"
-
             />
           </Box>
 
