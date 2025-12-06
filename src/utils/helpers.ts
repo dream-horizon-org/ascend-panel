2b975136bb97dff8a66d047509c40d3228122fbf
shyamsundar0212-D11 <shyamsundar.taddi@dream11.com>
2025-12-06 19:09:51 +0530

    chore: durations

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -28,8 +28,8 @@ export const formatNumber = (num?: number): string => {
 export const calculateDays = (startTime?: number, endTime?: number): number => {
   if (!startTime) return 0;
   try {
-    const start = startTime * 1000; // Convert to milliseconds
-    const end = endTime ? endTime * 1000 : Date.now();
+    const start = startTime ; // Convert to milliseconds
+    const end = endTime ? endTime : Date.now();
     const diffTime = Math.abs(end - start);
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
   } catch {
